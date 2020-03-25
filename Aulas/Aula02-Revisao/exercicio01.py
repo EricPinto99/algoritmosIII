11fbf2da150b4ce868c00f444398516bceb5a24c
EricPinto99 <ericgrandopinto@gmail.com>
2020-03-25 16:53:06 -0300

    added first exercise using methods

---
+++ Aulas/Aula02-Revisao/exercicio01.py
@@ -0,0 +1,9 @@
+x = int(input("Informe o valor de X: "))
+y = int(input("Informe o valor de Y: "))
+z = int(input("Informe o valor de Z: "))
+
+def calculcar():
+    expressao = (x - 5) * (y - z)
+    return expressao
+
+print(calculcar())
